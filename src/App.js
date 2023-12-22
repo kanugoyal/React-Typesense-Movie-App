9ba0ff3c5788a99b8d2232a3db5b340e2ed72332
Kanika <goyalkanu25@gmail.com>
2023-12-22 21:30:46 +0530

    typesense Data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import styled from "styled-components";
+import {
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from "react-instantsearch-dom";
+import { searchClient } from "./typesenseAdapter";
+import MoviesHits from "./components/moviesHits";
+import "instantsearch.css/themes/satellite.css";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em 0;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <h2>React/Typesense Movies InstantSearch</h2>
+      <InstantSearch indexName="movies" searchClient={searchClient}>
+        <h4>Search Movies</h4>
+        <SearchBox />
+        <RefinementList attribute="genres" />
+        <MoviesHits />
+        <Pagination />
+      </InstantSearch>
+    </AppContainer>
   );
 }
 
